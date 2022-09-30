e75b2d87730426bba68ea3108a85b64bf21712ef
Yavas20 <emreyavas20@hotmail.com>
2022-09-30 11:49:02 +0300

    initial and last commit

---
+++ src/Employee.java
@@ -0,0 +1,99 @@
+public class Employee {
+
+   private String fullName;
+   private double salary;
+   private int workHours;
+   private int hireYear;
+
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getWorkHours() {
+        return workHours;
+    }
+
+    public void setWorkHours(int workHours) {
+        this.workHours = workHours;
+    }
+
+    public int getHireYear() {
+        return hireYear;
+    }
+
+    public void setHireYear(int hireYear) {
+        this.hireYear = hireYear;
+    }
+
+    public Employee(String fullName, double salary, int workHours, int hireYear) {
+        setFullName(fullName);
+        setSalary(salary);
+        setWorkHours(workHours);
+        setHireYear(hireYear);
+    }
+
+    public double tax(double salary){
+
+        if(salary <= 1000){
+            return 0;
+        }
+        return salary * 0.03;
+
+    }
+
+    public int bonus(int workHours){
+
+        if(workHours <= 40) {
+            return 0;
+        }
+        return (workHours - 40) * 30;
+
+    }
+
+    public double raiseSalary(int hireYear){
+
+        int currentYear = 2021;
+
+        if((currentYear - hireYear) < 10 && (currentYear - hireYear) >=0){
+
+            return salary * 0.05;
+        } else if ((currentYear - hireYear) <= 19 && (currentYear - hireYear) >=10){
+
+            return salary * 0.1;
+        }else {
+
+            return salary * 0.15;
+        }
+
+    }
+
+
+
+
+    public String toString() {
+        return "Employee{" +
+                "fullName='" + fullName + '\'' +
+                ", salary=" + salary +
+                ", workHours=" + workHours +
+                ", hireYear=" + hireYear +
+                ", tax=" + tax(salary) +
+                ", bonus=" + bonus(workHours) +
+                ", raiseSalary=" + raiseSalary(hireYear) +
+                ", salaryWithoutTaxWithBonus=" + (salary - tax(salary) + bonus(workHours)) +
+                ", totalSalary=" + (salary - tax(salary) + bonus(workHours) + raiseSalary(hireYear)) +
+                '}';
+    }
+}
---
+++ src/EmployeeObjects.java
@@ -0,0 +1,19 @@
+public class EmployeeObjects {
+
+    public static void main(String[] args) {
+
+        Employee emp1 = new Employee("Emre Ankara", 3000, 46, 2020);
+
+        System.out.println(emp1);
+
+        Employee emp2 = new Employee("Pelin İstanbul", 800, 40, 2016);
+
+        System.out.println(emp2);
+
+        Employee emp3 = new Employee("Ali İzmir", 1600, 44, 2008);
+
+        System.out.println(emp3);
+
+
+    }
+}
