1b34e46f89082576816aa777cd3c4d17279d89b3
Yavas20 <emreyavas20@hotmail.com>
2022-11-02 10:21:15 +0300

    dik üçgende hipotenüs hesaplama

---
+++ src/hipotenus/dikUcgendeHipotenusHesaplama.java
@@ -0,0 +1,23 @@
+package hipotenus;
+
+import java.util.Scanner;
+
+public class dikUcgendeHipotenusHesaplama {
+    public static void main(String[] args) {
+
+        int a, b;
+        double c;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("1. Kenarı Giriniz : ");
+        a = scan.nextInt();
+
+        System.out.print("2. Kenarı Giriniz : ");
+        b = scan.nextInt();
+
+        c= Math.sqrt((a*a) + (b*b));
+
+        System.out.println("Hipotenüs : " + c);
+
+    }
+}
