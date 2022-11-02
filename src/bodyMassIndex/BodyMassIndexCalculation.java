786db3477aa7401250a49343929c98374929d012
Yavas20 <emreyavas20@hotmail.com>
2022-11-02 11:03:54 +0300

    body mass index calculation

---
+++ src/bodyMassIndex/BodyMassIndexCalculation.java
@@ -0,0 +1,22 @@
+package bodyMassIndex;
+
+import java.util.Scanner;
+
+public class BodyMassIndexCalculation {
+    public static void main(String[] args) {
+
+        double heightMeter, massIndex;
+        int weight;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your height in meter : ");
+        heightMeter = scan.nextDouble();
+
+        System.out.print("Enter your wight in kg : ");
+        weight = scan.nextInt();
+
+        massIndex = weight / (heightMeter * heightMeter);
+        System.out.println(massIndex);
+
+    }
+}
